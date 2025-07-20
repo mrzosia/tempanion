8d63d6f642006eaa324d717e589bdc37751d3494
mrzosia <mrzosia117@outlook.com>
2025-07-20 01:38:37 -0400

    Added scroll to top button for temtem list page

--- js/temtem.js
+++ js/temtem.js
@@ -73,3 +73,20 @@ window.addEventListener("load", () => {
             });
     }
 });
+
+const scrollToTopBtn = document.getElementById('scrollToTop');
+
+window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 300) {
+        scrollToTopBtn.classList.add('visible');
+    } else {
+        scrollToTopBtn.classList.remove('visible');
+    }
+});
+
+scrollToTopBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
